e453d6d7716286ad2ff6fb640cf0f0fdfc08ad52
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-08-02 12:22:43 +0100

    Creating environment, creating database and tables in seed

--- db/connection.js
+++ db/connection.js
@@ -3,11 +3,14 @@ const path = require('path');
 const ENV = process.env.NODE_ENV || 'development';
 
 require('dotenv').config({
-  path: path.resolve(__dirname, `../.env.${ENV}`),
+    path: path.resolve(__dirname, `../.environment-variables/.env.${ENV}`),
 });
 
+
 if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+    throw new Error('PGDATABASE not set');
 }
+
+console.log(`${process.env.PGDATABASE} is active`)
 
 module.exports = new Pool();
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,7 +1,41 @@
-const seed = (data) => {
-  const { articleData, commentData, topicData, userData } = data;
-  // 1. create tables
-  // 2. insert data
+const db = require("../connection.js")
+
+const seed = async(data) => {
+    const { articleData, commentData, topicData, userData } = data;
+    // 1. create tables
+    await db.query("DROP TABLE IF EXISTS topics;");
+    await db.query("DROP TABLE IF EXISTS articles;");
+    await db.query("DROP TABLE IF EXISTS users;");
+    await db.query("DROP TABLE IF EXISTS comments;");
+    await db.query(`CREATE TABLE topics (
+        slug TEXT PRIMARY KEY NOT NULL,
+        description TEXT
+    );`);
+    await db.query(`CREATE TABLE users (
+        username VARCHAR(40)PRIMARY KEY NOT NULL,
+        avatar_url VARCHAR,
+        name VARCHAR(60) NOT NULL,
+    )`)
+    await db.query(`CREATE TABLE article (
+        article_id SERIAL NOT NULL PRIMARY KEY,
+        title VARCHAR(127) NOT NULL,
+        body TEXT,
+        votes INT NOT NULL DEFAULT 0,
+        topic FOREIGN ID REFERENCES topics (slug) NOT NULL,
+        author FOREIGN ID REFERENCES users (username) NOT NULL,
+        created_at TIME DEFAULT CURRENT_TIME,
+        created_on DATE DEFAULT CURRENT_DATE
+    )`)
+    await db.query(`CREATE TABLE comments (
+        comment_id SERIAL PRIMARY KEY NOT NULL,
+        author FOREIGN ID REFERENCES users (username) NOT NULL,
+        article_id FOREIGN ID REFERENCES articles (article_id) NOT NULL,
+        votes INT DEFAULT 0,
+        created_at TIME DEFAULT CURRENT_TIME,
+        created_on DATE DEFAULT CURRENT_DATE,
+        body TEXT NOT NULL
+    )`)
+        // 2. insert data
 };
 
 module.exports = seed;
