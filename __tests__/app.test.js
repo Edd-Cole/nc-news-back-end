b98f644aaafa5f23e281658f8ca0bba1a4b875c0
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-08-03 14:17:31 +0100

    Restructuring app.test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,35 +10,39 @@ afterAll(() => db.end());
 describe("/api", () => {
     describe("/topics", () => {
         describe("/ - GET", () => {
-            test("status 200 - returns all the topics", () => {
-                return request(app).get("/api/topics").expect(200)
-                    .then((response) => {
-                        response.body.topics.forEach(topic => {
-                            expect(topic).toMatchObject({
-                                slug: expect.any(String),
-                                description: expect.any(String)
+            describe("status 200 - Success", () => {
+                test("returns all the topics", () => {
+                    return request(app).get("/api/topics").expect(200)
+                        .then((response) => {
+                            response.body.topics.forEach(topic => {
+                                expect(topic).toMatchObject({
+                                    slug: expect.any(String),
+                                    description: expect.any(String)
+                                })
                             })
                         })
-                    })
+                })
             })
         })
     })
 
     describe("/users", () => {
         describe("/ - GET", () => {
-            test("status 200 - returns all the users", () => {
-                return request(app).get("/api/users").expect(200)
-                    .then(response => {
-                        response.body.users.forEach(user => {
-                            expect(user).toMatchObject({
-                                username: expect.any(String),
-                                avatar_url: expect.any(String),
-                            })
-                            expect(user).not.toMatchObject({
-                                name: expect.any(String)
+            describe("status 200 - Success", () => {
+                test("returns all the users", () => {
+                    return request(app).get("/api/users").expect(200)
+                        .then(response => {
+                            response.body.users.forEach(user => {
+                                expect(user).toMatchObject({
+                                    username: expect.any(String),
+                                    avatar_url: expect.any(String),
+                                })
+                                expect(user).not.toMatchObject({
+                                    name: expect.any(String)
+                                })
                             })
                         })
-                    })
+                })
             })
         })
     })
@@ -61,39 +65,46 @@ describe("/api", () => {
                     })
             })
         })
+
         describe("/:article_id - GET", () => {
-            test("status 200 - returns a specific article", () => {
-                return request(app).get("/api/articles/5").expect(200)
-                    .then(response => {
-                        expect(response.body.articles[0]).toMatchObject({
-                            article_id: 5,
-                            title: expect.any(String),
-                            body: expect.any(String),
-                            votes: expect.any(Number),
-                            topic: expect.any(String),
-                            author: expect.any(String),
+            describe("status 200 - Success", () => {
+                test("returns a specific article", () => {
+                    return request(app).get("/api/articles/5").expect(200)
+                        .then(response => {
+                            expect(response.body.articles[0]).toMatchObject({
+                                article_id: 5,
+                                title: expect.any(String),
+                                body: expect.any(String),
+                                votes: expect.any(Number),
+                                topic: expect.any(String),
+                                author: expect.any(String),
+                            })
                         })
-                    })
+                })
             })
 
-            test("status 404 - returns an error when id is not present", () => {
-                return request(app).get("/api/articles/100000").expect(404)
-                    .then(response => {
-                        expect(response.body.msg).toBe("article_id does not exist")
-                    })
+            describe("status 400 - Bad Request", () => {
+                test("returns an error when id is not of proper type", () => {
+                    return request(app).get("/api/articles/dog").expect(400)
+                        .then(response => {
+                            expect(response.body.msg).toBe("article_id is not of correct type")
+                        })
+                })
             })
 
-            test("status 400 - returns an error when id is not of proper type", () => {
-                return request(app).get("/api/articles/dog").expect(400)
-                    .then(response => {
-                        expect(response.body.msg).toBe("article_id is not of correct type")
-                    })
+            describe("status 404 - Page Not Found", () => {
+                test("returns an error when id is not present", () => {
+                    return request(app).get("/api/articles/100000").expect(404)
+                        .then(response => {
+                            expect(response.body.msg).toBe("article_id does not exist")
+                        })
+                })
             })
         })
 
         describe("/:article_id - PATCH", () => {
             describe("status 200 - Success", () => {
-                test("status 200 - updates only one part of an article", () => {
+                test("updates only one part of an article", () => {
                     return request(app).patch("/api/articles/1").send({ title: "Bing" }).expect(200)
                         .then(response => {
                             expect(response.body.articles[0]).toMatchObject({
@@ -107,7 +118,7 @@ describe("/api", () => {
                         })
                 })
 
-                test("status 200 - can update any part of an article and multiple fields", () => {
+                test("can update any part of an article and multiple fields", () => {
                     return request(app).patch("/api/articles/1").send({ title: "Bing", body: "Bong", votes: 17, topic: "cats", author: "rogersop" }).expect(200)
                         .then(response => {
                             expect(response.body.articles[0]).toMatchObject({
@@ -121,7 +132,7 @@ describe("/api", () => {
                         })
                 })
 
-                test("status 200 - returns article when when only invalid keys are used", () => {
+                test("returns article when only invalid keys are used", () => {
                     return request(app).patch("/api/articles/1").send({ subtitle: "Not valid" }).expect(200)
                         .then(response => {
                             expect(response.body.articles[0]).toMatchObject({
@@ -138,7 +149,7 @@ describe("/api", () => {
                         })
                 })
 
-                test("status 200 - updates only the relevant data when passed a mix of valid and invalid keys", () => {
+                test("updates only the relevant data when passed a mix of valid and invalid keys", () => {
                     return request(app).patch("/api/articles/1").send({ title: "Bingo", subtitle: "Not valid" }).expect(200)
                         .then(response => {
                             expect(response.body.articles[0]).toMatchObject({
@@ -157,21 +168,21 @@ describe("/api", () => {
             })
 
             describe("status 400 - Bad Request", () => {
-                test("status 400 - returns an error when passed an invalid type of article_id", () => {
+                test("returns an error when passed an invalid type of article_id", () => {
                     return request(app).patch("/api/articles/badarticle_id").send({ title: "Bingo" }).expect(400)
                         .then(response => {
                             expect(response.body.msg).toBe("invalid type for key")
                         })
                 })
 
-                test("status 400 - updating foreign key with incorrect info", () => {
+                test("updating foreign key with incorrect info", () => {
                     return request(app).patch("/api/articles/1").send({ topic: "Not a valid topic" }).expect(400)
                         .then(response => {
                             expect(response.body.msg).toBe("cannot create an original value for topic and/or author")
                         })
                 })
 
-                test("status 400 - patched info is too large for columns", () => {
+                test("patched info is too large for columns", () => {
                     return request(app).patch("/api/articles/1").send({ title: "I have no idea how long this title will go on for, I really really really really really hope that it will reach 128 characters long soon, otherwise this will be absolutely ridiculously long. I cannot believe that I am still typing this out, I must have hit 128 characters now, surely!" })
                         .expect(400)
                         .then(response => {
@@ -179,14 +190,14 @@ describe("/api", () => {
                         })
                 })
 
-                test("status 400 - invalid type of info to update", () => {
+                test("invalid type of info to update", () => {
                     return request(app).patch("/api/articles/1").send({ votes: "not a valid type" }).expect(400)
                         .then(response => {
                             expect(response.body.msg).toBe("invalid type for key")
                         })
                 })
 
-                test("status 400 - is not vulnerable to SQL Injection", () => {
+                test("is not vulnerable to SQL Injection", () => {
                     return request(app).patch("/api/articles/1").send({ body: "something; DROP TABLE articles;" })
                         .then(response => {
                             expect(response.body.articles).not.toHaveLength(0)
@@ -203,7 +214,7 @@ describe("/api", () => {
             })
 
             describe("status 404 - Page Not Found", () => {
-                test("status 404 - article_id is a number but does not exist", () => {
+                test("article_id is a number but does not exist", () => {
                     return request(app).patch("/api/articles/100000").send({ title: "Bingo" }).expect(404)
                         .then(response => {
                             expect(response.body.msg).toBe("article does not exist")
@@ -215,19 +226,21 @@ describe("/api", () => {
 
     describe("/comments", () => {
         describe("/ - GET", () => {
-            test("status 200 - returns all the comments", () => {
-                return request(app).get("/api/comments").expect(200)
-                    .then(response => {
-                        response.body.comments.forEach(comment => {
-                            expect(comment).toMatchObject({
-                                comment_id: expect.any(Number),
-                                author: expect.any(String),
-                                title: expect.any(String),
-                                votes: expect.any(Number),
-                                body: expect.any(String)
+            describe("status 200 - Success", () => {
+                test("returns all the comments", () => {
+                    return request(app).get("/api/comments").expect(200)
+                        .then(response => {
+                            response.body.comments.forEach(comment => {
+                                expect(comment).toMatchObject({
+                                    comment_id: expect.any(Number),
+                                    author: expect.any(String),
+                                    title: expect.any(String),
+                                    votes: expect.any(Number),
+                                    body: expect.any(String)
+                                })
                             })
                         })
-                    })
+                })
             })
         })
     })
