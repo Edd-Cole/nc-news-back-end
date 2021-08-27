c10f57d8f280bfc23ecfaf2c1595da085e61c529
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-08-27 11:45:21 +0100

    Adding cors functionality to backend

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const apiRouter = require("./routers/routes/api-router.js");
 const { apiError, psqlError, fatalError } = require("./routers/error-routes/api-errors.js");
+const cors = require("cors");
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 app.use("/api", apiRouter);
